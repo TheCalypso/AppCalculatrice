99eed455c33429b4f2b141fb2b3fd6752294693f
TheCalypso <bastien.dorangeon.penichot@gmail.com>
2024-06-03 18:06:11 +0200

    add logic +,-,*,/

--- app/src/main/java/fr/gaspezia/calculatrice/MainActivity.java
+++ app/src/main/java/fr/gaspezia/calculatrice/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.gaspezia.calculatrice;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,84 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Double firstNumber = null;
+    private String currentOperator = null;
+    private TextView resultTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        resultTextView = findViewById(R.id.display);
+
+        findViewById(R.id.button0).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "0"));
+        findViewById(R.id.button1).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "1"));
+        findViewById(R.id.button2).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "2"));
+        findViewById(R.id.button3).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "3"));
+        findViewById(R.id.button4).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "4"));
+        findViewById(R.id.button5).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "5"));
+        findViewById(R.id.button6).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "6"));
+        findViewById(R.id.button7).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "7"));
+        findViewById(R.id.button8).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "8"));
+        findViewById(R.id.button9).setOnClickListener(v -> resultTextView.setText(resultTextView.getText() + "9"));
+        findViewById(R.id.ce).setOnClickListener(v -> {
+            firstNumber = null;
+            currentOperator = null;
+            resultTextView.setText("");
+        });
+        findViewById(R.id.dot).setOnClickListener(v -> {
+            if (!resultTextView.getText().toString().contains(".")) {
+                resultTextView.setText(resultTextView.getText() + ".");
+            }
+        });
+
+        findViewById(R.id.minus).setOnClickListener(v -> {
+            firstNumber = Double.valueOf(resultTextView.getText().toString());
+            currentOperator = "-";
+            resultTextView.setText("");
+        });
+
+        findViewById(R.id.plus).setOnClickListener(v -> {
+            firstNumber = Double.valueOf(resultTextView.getText().toString());
+            currentOperator = "+";
+            resultTextView.setText("");
+        });
+
+        findViewById(R.id.asterisk).setOnClickListener(v -> {
+            firstNumber = Double.valueOf(resultTextView.getText().toString());
+            currentOperator = "*";
+            resultTextView.setText("");
+        });
+
+        findViewById(R.id.slash).setOnClickListener(v -> {
+            firstNumber = Double.valueOf(resultTextView.getText().toString());
+            currentOperator = "/";
+            resultTextView.setText("");
+        });
+
+        findViewById(R.id.equal).setOnClickListener(v -> {
+            if (firstNumber != null && currentOperator != null) {
+                double secondNumber = Double.parseDouble(resultTextView.getText().toString());
+                if (currentOperator.equals("+")) {
+                    double result = firstNumber + secondNumber;
+                    resultTextView.setText(String.valueOf(result));
+                }
+                if (currentOperator.equals("*")) {
+                    double result = firstNumber * secondNumber;
+                    resultTextView.setText(String.valueOf(result));
+                }
+                if (currentOperator.equals("-")) {
+                    double result = firstNumber - secondNumber;
+                    resultTextView.setText(String.valueOf(result));
+                }
+                if (currentOperator.equals("/")) {
+                    double result = firstNumber / secondNumber;
+                    resultTextView.setText(String.valueOf(result));
+                }
+                firstNumber = null;
+                currentOperator = null;
+            }
         });
     }
 }
